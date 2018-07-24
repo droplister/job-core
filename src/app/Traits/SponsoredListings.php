0ad3cd7b065c51b23ce1878326e5dc875f04f71f
Dan Anderson <anderson@familymedia.org>
2018-07-24 11:54:00 -0400

    Update

--- src/app/Traits/SponsoredListings.php
+++ src/app/Traits/SponsoredListings.php
@@ -14,6 +14,8 @@ trait SponsoredListings
     {
         $keyword = isset($this->value) ? $this->value : $this->name;
 
+        $keyword = $keyword . ' ' . config('job-core.keyword_root');
+
         $query = new \JobApis\Jobs\Client\Queries\JujuQuery([
             'partnerid' => config('job-core.partner_id')
         ]);
