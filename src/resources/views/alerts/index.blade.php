5c795cfbb529b13903b2027c8e5abc74220f3412
Dan Anderson <anderson@familymedia.org>
2018-07-24 14:08:42 -0400

    Update

--- src/resources/views/alerts/index.blade.php
+++ src/resources/views/alerts/index.blade.php
@@ -9,61 +9,6 @@
     <div class="my-3 p-3 bg-white rounded box-shadow">
         <div class="row">
             <div class="col-md-9">
-                <h6 class="border-bottom border-gray pb-2 mb-0">Resources</h6>
-                @if(config('job-core.filter') === 'security_clearance')
-                    <p class="text-muted small lh-135 pt-3 mb-0">
-                        <a href="https://www.reddit.com/r/SecurityClearance/" target="_blank">
-                            <strong>Reddit</strong>
-                        </a> - https://www.reddit.com/r/SecurityClearance/
-                    </p>
-                    <p class="text-muted small lh-135 pt-2 mb-0">
-                        <a href="https://nbib.opm.gov/e-qip-background-investigations" target="_blank">
-                            <strong>e-QIP</strong>
-                        </a> - Web-based system to facilitate electronic transmission of investigative forms.
-                    </p>
-                    <p class="text-muted small lh-135 pt-2 mb-0">
-                        <a href="https://www.opm.gov/forms/pdf_fill/sf86-non508.pdf" target="_blank">
-                            <strong>SF-86</strong>
-                        </a> - Questionnaire for National Security Positions fillable PDF form.
-                    </p>
-                    <p class="text-muted small lh-135 pt-2 mb-0">
-                        <a href="https://www.opm.gov/forms/pdf_fill/sf85.pdf" target="_blank">
-                            <strong>SF-85</strong>
-                        </a> - Questionnaire for Non-Sensitive Positions fillable PDF form.
-                    </p>
-                    <p class="text-muted small lh-135 pt-2 mb-0">
-                        <a href="https://www.opm.gov/forms/pdf_fill/sf85p.pdf" target="_blank">
-                            <strong>SF-85P</strong>
-                        </a> - Questionnaire for Public Trust Positions fillable PDF form.
-                    </p>
-                    <p class="text-muted small lh-135 pt-2 pb-2 mb-0">
-                        <a href="http://www.dhra.mil/Portals/52/Documents/perserec/ADR_Version_4.pdf" target="_blank">
-                            <strong>Adjudicative Desk Reference</strong>
-                        </a> - A comprehensive collection of background information on the 13 categories of behavior considered when determining an individual’s eligibility for a security clearance.
-                    </p>
-                @elseif(config('job-core.filter') === 'federal')
-                    <p class="text-muted small lh-135 pt-3 mb-0">
-                        <a href="https://www.youtube.com/watch?v=8YX7o1PBoFk">
-                            VIDEO from USAJOBS.gov on writing your resume
-                        </a>
-                    </p>
-                    <p class="text-muted small lh-135 pt-2 mb-0">
-                        <a href="https://my.usajobs.gov/Content/pdfs/Tutorials-Resume.pdf">
-                            Tutorial on creating a resume in USAJobs (PDF)
-                        </a>
-                    </p>
-                    <p class="text-muted small lh-135 pt-2 mb-0">
-                        <a href="https://www.usajobs.gov/Help/faq/application/documents/resume/what-to-include/">
-                            Resume and application tips
-                        </a>
-                    </p>
-                    <p class="text-muted small lh-135 pt-2 pb-2 mb-0">
-                        <a href="https://www.govloop.com/resources/10-tips-for-letting-federal-employers-know-your-worth-infographic/">
-                            Tips on describing your experience and skills
-                        </a>
-                    </p>
-                @endif
-                <br class="d-md-none" />
             </div>
             <div class="col-md-3">
                 @include('job-core::partials.h-tag', [
