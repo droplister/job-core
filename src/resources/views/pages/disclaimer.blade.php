875ecb404e22569c36e1abf5eea77c3b302f7327
Dan Anderson <anderson@familymedia.org>
2018-07-24 02:35:19 -0400

    Update Disclaimer

--- src/resources/views/pages/disclaimer.blade.php
+++ src/resources/views/pages/disclaimer.blade.php
@@ -14,9 +14,9 @@
                 <h6 class="border-bottom border-gray pb-2 mb-0">
                     Disclaimers
                 </h6>
-                <p class="text-muted small lh-135 mt-3">1. {{ config('job-core.domain') }} is a privately owned website. Family Media LLC publishes free internet resources for niche audiences online. This website is not endorsed by any government agency.</p>
-                <p class="text-muted small lh-135">2. The majority of our job listings come from <a href="https://www.usajobs.gov/" target="_blank">USAJobs.gov</a>. We try our best to offer the most up-to-date information, but we cannot guarantee the accuracy of information posted on {{ config('job-core.domain') }}.</p>
-                <p class="text-muted small lh-135">3. This site generates income through banner advertisements and affiliate links. Third party vendors, including Google, use cookies to serve ads based on a user's prior visits to our website. </p>
+                <p class="text-muted small lh-135 mt-3">1. This website is not endorsed by, or associated with, any government agency. {{ config('job-core.domain') }} is privately owned by <a href="https://familymediallc.com/">Family Media LLC</a>. Family Media publishes free resources for niche audiences online.</p>
+                <p class="text-muted small lh-135">2. The listings on our website come from <a href="https://www.usajobs.gov/" target="_blank">USAJobs.gov</a> and other job boards. We try our best to offer the most up-to-date information, but we cannot guarantee the accuracy of any information posted on {{ config('job-core.domain') }}.</p>
+                <p class="text-muted small lh-135">3. This site generates income through banner advertisements and affiliate links. Third party vendors, including Google, use cookies to serve ads based on a user's prior visits to our website.</p>
             </div>
             <div class="col-12 col-md-3">
             </div>
