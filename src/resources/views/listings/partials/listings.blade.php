346b1a90ddeeabd0f442df65cb012d9903f12600
Dan Anderson <anderson@familymedia.org>
2018-07-24 12:41:26 -0400

    Update

--- src/resources/views/listings/partials/listings.blade.php
+++ src/resources/views/listings/partials/listings.blade.php
@@ -1,14 +1,14 @@
 @include('job-core::listings.partials.listings-header')
 
-@if(isset($sponsored))
-	@foreach($sponsored->all() as $job)
-	    @include('job-core::listings.partials.listing-sponsored')
-	    @if($loop->iteration === 1) @break @endif
-	@endforeach
-@endif
-
 @foreach($listings as $listing)
     @include('job-core::listings.partials.listing')
 @endforeach
+
+@if(isset($sponsored))
+	@foreach($sponsored->all() as $job)
+	    @include('job-core::listings.partials.listing-sponsored')
+	    @if($loop->iteration === 2) @break @endif
+	@endforeach
+@endif
 
 @include('job-core::listings.partials.listings-footer')
