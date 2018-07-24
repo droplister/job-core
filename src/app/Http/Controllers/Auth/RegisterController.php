e28e92843bf51ef7629c2a89bee8365734aa9b74
Dan Anderson <anderson@familymedia.org>
2018-07-24 00:22:07 -0400

    Update

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -62,7 +62,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return \App\User::create([
+        return \Droplister\JobCore\App\User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
