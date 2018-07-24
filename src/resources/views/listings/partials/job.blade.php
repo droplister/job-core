712f2594b07a444d19cc781955c09b2cc28d8c7a
Dan Anderson <anderson@familymedia.org>
2018-07-24 10:50:19 -0400

    Protected Properties

--- src/resources/views/listings/partials/job.blade.php
+++ src/resources/views/listings/partials/job.blade.php
@@ -1,1 +1,22 @@
-111111
+<p class="text-muted pt-3 pb-3 mb-0 small lh-135 border-bottom border-gray bg-warning">
+    <a href="{{ $job->getUrl() }}" class="d-block font-weight-bold" target="_blank">
+        {{ $job->getName() }}
+    </a>
+    <span class="d-block">
+        {{ $job->getCompany() }}
+        -
+        {{ $job->getLocation() }}
+    </span>
+    <span class="d-block text-success">
+        Sponsored Job Listing
+    </span>
+    <span class="d-block mt-1">
+        {{ $job->getDescription() }}
+    </span>
+    <span class="d-block mt-1">
+        Published {{ $job->getDatePosted() }}
+        <a href="{{ $job->getUrl() }}" class="ml-1" target="_blank">
+            <i class="fa fa-external-link"></i> Apply Now
+        </a>
+    </span>
+  </p>
--- src/resources/views/listings/partials/jobs.blade.php
+++ src/resources/views/listings/partials/jobs.blade.php
@@ -1,9 +1,3 @@
-1
-@foreach($jobs->items as $job)
+@foreach($jobs->getItems() as $job)
     @include('job-core::listings.partials.job')
 @endforeach
-2
-@foreach($jobs->items() as $job)
-    @include('job-core::listings.partials.job')
-@endforeach
-3
