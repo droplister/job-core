b0b2fda385de0b5d853488f3345bac1b31e2b6bb
Dan Anderson <anderson@familymedia.org>
2018-07-24 16:39:31 -0400

    Update

--- src/resources/views/alerts/index.blade.php
+++ src/resources/views/alerts/index.blade.php
@@ -3,70 +3,70 @@
 @section('title', str_singular(config('job-core.keyword')) . ' Alerts')
 
 @section('content')
-    <section class="jumbotron text-center mt-3 {{ config('job-core.body_class') }}">
-        <div class="container">
-            <h1 class="jumbotron-heading">
-                <i class="fa fa-bell"></i>
-                Email Alerts
-            </h1>
-            <p class="lead text-muted">Get instantly notified of new jobs.</p>
-            <p class="lead text-muted mb-1">
-                <a href="{{ route('alerts.create') }}" class="btn {{ config('job-core.button_class') }}">
-                    Create an Alert
-                </a>
-            </p>
-            <p class="lead text-muted small"><em>ad free experience</em></p>
-        </div>
-    </section>
-    <div class="my-3 p-3 bg-white rounded box-shadow">
-        <div class="row">
-            <div class="col-md-9">
-                @include('job-core::partials.h-tag', [
-                    'tag' => 'h6',
-                    'title' => 'My Alerts',
-                ])
-                @if(count($alerts))
-                    Show Alerts
-                @else
-                    <div class="alert alert-warning mt-3 box-shadow">
-                        <a href="{{ route('alerts.create') }}">
-                            <i class="fa fa-bell"></i> Create an Alert
-                        </a>
-                    </div>
-                @endif
-                @include('job-core::partials.resources')
+    @if(count($alerts) === 0)
+        <section class="jumbotron text-center mt-3 {{ config('job-core.body_class') }}">
+            <div class="container">
+                <h1 class="jumbotron-heading">
+                    <i class="fa fa-bell"></i>
+                    Email Alerts
+                </h1>
+                <p class="lead text-muted">Get instantly notified of new jobs.</p>
+                <p class="lead text-muted mb-1">
+                    <a href="{{ route('alerts.create') }}" class="btn {{ config('job-core.button_class') }}">
+                        Create an Alert
+                    </a>
+                </p>
+                <p class="lead text-muted small"><em>ad free experience</em></p>
             </div>
-            <div class="col-md-3">
-                @include('job-core::partials.h-tag', [
-                    'tag' => 'h6',
-                    'title' => 'Profile',
-                ])
-                @include('job-core::partials.p-tag', [
-                    'text' => $user->name,
-                    'pt' => 'pt-3',
-                    'pb' => '',
-                ])
-                @include('job-core::partials.p-tag', [
-                    'text' => $user->email,
-                    'pt' => '',
-                    'pb' => '',
-                ])
-                @include('job-core::partials.p-tag', [
-                    'text' => 'Joined: ' . $user->created_at->toDateString(),
-                    'pt' => '',
-                    'pb' => 'pb-4',
-                ])
-                <a href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                    <i class="fa fa-sign-out"></i>
-                    {{ __('Logout') }}
-                </a>
+        </section>
+    @else
+        @include('job-core::partials.title', [
+            'fa' => 'fa-bell',
+            'title' => 'Alerts',
+            'link' => route('alerts.index')
+        ])
+        <div class="my-3 p-3 bg-white rounded box-shadow">
+            <div class="row">
+                <div class="col-md-9">
+                    @include('job-core::partials.h-tag', [
+                        'tag' => 'h6',
+                        'title' => 'My Alerts',
+                    ])
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
+                    @include('job-core::partials.resources')
+                </div>
+                <div class="col-md-3">
+                    @include('job-core::partials.h-tag', [
+                        'tag' => 'h6',
+                        'title' => 'Profile',
+                    ])
+                    @include('job-core::partials.p-tag', [
+                        'text' => $user->name,
+                        'pt' => 'pt-3',
+                        'pb' => '',
+                    ])
+                    @include('job-core::partials.p-tag', [
+                        'text' => $user->email,
+                        'pt' => '',
+                        'pb' => '',
+                    ])
+                    @include('job-core::partials.p-tag', [
+                        'text' => 'Joined: ' . $user->created_at->toDateString(),
+                        'pt' => '',
+                        'pb' => 'pb-4',
+                    ])
+                    <a href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                        <i class="fa fa-sign-out"></i>
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
+    @endif
 @endsection
