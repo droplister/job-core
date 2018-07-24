c811866ebcb2de37ca5ffecc18c881828168bec5
Dan Anderson <anderson@familymedia.org>
2018-07-23 21:09:07 -0400

    Fix Class Name Typo

--- src/database/migrations/2017_05_31_000010_create_listing_agency_sub_element_pivot_table.php
+++ src/database/migrations/2017_05_31_000010_create_listing_agency_sub_element_pivot_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAgencySubElementListingPivotTable extends Migration
+class CreateListingAgencySubElementPivotTable extends Migration
 {
     /**
      * Run the migrations.
