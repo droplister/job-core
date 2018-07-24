e78a64214c16bfed01a097514efd0b8db0c4abb2
Dan Anderson <anderson@familymedia.org>
2018-07-24 10:31:24 -0400

    Update

--- src/resources/views/listings/partials/jobs.blade.php
+++ src/resources/views/listings/partials/jobs.blade.php
@@ -1,3 +1,9 @@
+1
 @foreach($jobs->items as $job)
     @include('job-core::listings.partials.job')
 @endforeach
+2
+@foreach($jobs->items() as $job)
+    @include('job-core::listings.partials.job')
+@endforeach
+3
