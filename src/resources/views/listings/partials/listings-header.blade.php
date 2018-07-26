61f31c236c4c124e464af9f46e583ec2b986d44a
Dan Anderson <anderson@familymedia.org>
2018-07-26 11:04:54 -0400

    Listing Title

--- src/resources/views/listings/partials/listings-header.blade.php
+++ src/resources/views/listings/partials/listings-header.blade.php
@@ -2,16 +2,17 @@
     @include('job-core::partials.h-tag', [
         'tag' => 'h6',
         'title' => 'Related Listings',
-        'mt' => isset($sponsored) ? 'mt-4' : '',
     ])
 @elseif($listings->currentPage() === 1)
     @include('job-core::partials.h-tag', [
         'tag' => 'h6',
         'title' => $listings->total() . ' ' . str_plural('Job', $listings->total()) . ' Found',
+        'mt' => isset($sponsored) ? 'mt-4' : '',
     ])
 @else
     @include('job-core::partials.h-tag', [
         'tag' => 'h6',
         'title' => 'Page ' . $listings->currentPage() . ' of ' . $listings->lastPage(),
+        'mt' => isset($sponsored) ? 'mt-4' : '',
     ])
 @endif
