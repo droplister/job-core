d6694fbc53f6f9c3e69897d9b9eea739435a197d
Dan Anderson <anderson@familymedia.org>
2018-07-27 15:38:30 -0400

    Coming Soon

--- src/resources/views/alerts/create.blade.php
+++ src/resources/views/alerts/create.blade.php
@@ -1,5 +1,13 @@
 @extends('job-core::layouts.app')
 
 @section('content')
-
+    <section class="jumbotron text-center mt-3 {{ config('job-core.body_class') }}">
+        <div class="container">
+            <h1 class="jumbotron-heading">
+                <i class="fa fa-bell"></i>
+                Email Alerts
+            </h1>
+            <p class="lead text-muted">Thank you for your interest in our job alerts feature. It is under going maintenance and is expected to be available again soon.</p>
+        </div>
+    </section>
 @endsection
