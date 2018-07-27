e286964185472cde86433c778b7ea32310961247
Dan Anderson <anderson@familymedia.org>
2018-07-27 11:47:18 -0400

    Bold Links

--- src/resources/views/partials/resources.blade.php
+++ src/resources/views/partials/resources.blade.php
@@ -4,22 +4,22 @@
     'mt' => 'mt-4',
 ])
 <p class="text-muted small lh-135 pt-3 mb-0">
-    <a href="https://www.youtube.com/watch?v=8YX7o1PBoFk">
+    <a href="https://www.youtube.com/watch?v=8YX7o1PBoFk" class="font-weight-bold">
         VIDEO from USAJOBS.gov on writing your resume
     </a>
 </p>
 <p class="text-muted small lh-135 pt-2 mb-0">
-    <a href="https://my.usajobs.gov/Content/pdfs/Tutorials-Resume.pdf">
+    <a href="https://my.usajobs.gov/Content/pdfs/Tutorials-Resume.pdf" class="font-weight-bold">
         Tutorial on creating a resume in USAJobs (PDF)
     </a>
 </p>
 <p class="text-muted small lh-135 pt-2 mb-0">
-    <a href="https://www.usajobs.gov/Help/faq/application/documents/resume/what-to-include/">
+    <a href="https://www.usajobs.gov/Help/faq/application/documents/resume/what-to-include/" class="font-weight-bold">
         Resume and application tips
     </a>
 </p>
 <p class="text-muted small lh-135 pt-2 pb-2 mb-0">
-    <a href="https://www.govloop.com/resources/10-tips-for-letting-federal-employers-know-your-worth-infographic/">
+    <a href="https://www.govloop.com/resources/10-tips-for-letting-federal-employers-know-your-worth-infographic/" class="font-weight-bold">
         Tips on describing your experience and skills
     </a>
 </p>
