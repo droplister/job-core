eab5c180751bdb64d8667a167de6219fac8eb8c1
Dan Anderson <anderson@familymedia.org>
2018-07-30 11:14:41 -0400

    Update Title

--- src/resources/views/partials/title.blade.php
+++ src/resources/views/partials/title.blade.php
@@ -1,20 +1,20 @@
 <div class="d-flex align-items-center p-3 my-3 rounded box-shadow {{ config('job-core.title_class') }}">
     <i class="fa {{ $fa }} fa-lg mt-3 mr-3 mb-3"></i>
     <div class="lh-100">
-        @if(isset($link))
-            <h1 class="h6 mb-0 lh-100">
-                <a href="{{ $link }}" class="text-white">
+        <h1 class="h6 mb-0 lh-100">
+            @if(isset($link))
+                <a href="{{ $link }}" class="text-white d-block">
                     {{ $title }}
                 </a>
-            </h1>
-        @else
-            <h1 class="h6 mb-0 text-white lh-100">
-                {{ $title }}
-            </h1>
-        @endif
-        @if(isset($subtitle))
-            <small>{{ $subtitle }}</small>
-        @endif
+            @else
+                <span class="text-white d-block">
+                    {{ $title }}
+                </span>
+            @endif
+            @if(isset($subtitle))
+                <small>{{ $subtitle }}</small>
+            @endif
+        </h1>
     </div>
 </div>
 @include('job-core::partials.adsense')
