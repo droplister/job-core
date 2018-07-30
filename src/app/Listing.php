e13bf46723e7f3c65376de1ecfc9705e45019354
Dan Anderson <anderson@familymedia.org>
2018-07-30 12:49:20 -0400

    Tweatk Filter

--- src/app/Listing.php
+++ src/app/Listing.php
@@ -243,12 +243,9 @@ class Listing extends Model
     {
         return Cache::rememberForever('listing_' . $this->slug . '_position_title',
             function () use ($value) {
-                if (strpos($value, ' ' . $this->job_grade) === false &&
-                    strpos($value, ' ' . $this->job_grade_code . '-') === false && 
-                    strpos($value, ' ' . $this->job_grade_code) === false ||
-                    strpos($value, '(' . $this->job_grade_code . '-') === false && 
-                    strpos($value, '(' . $this->job_grade) === false && 
-                    strpos($value, '(' . $this->job_grade_code) === false)
+                if (strpos($value, $this->job_grade) === false &&
+                    strpos($value, $this->job_grade_code . '-') === false && 
+                    strpos($value, $this->job_grade_code) === false)
                 {
                     return trim($value) . ', ' . $this->job_grade;
                 }
