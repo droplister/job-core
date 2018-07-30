bdb366748e3415860597ee96eaf3f1c5a94f1b1d
Dan Anderson <anderson@familymedia.org>
2018-07-30 10:42:57 -0400

    H Tag

--- src/resources/views/partials/h-tag.blade.php
+++ src/resources/views/partials/h-tag.blade.php
@@ -1,4 +1,4 @@
-<{{ $tag }} class="border-bottom border-gray{{ isset($pt) ? ' ' . $pt : '' }}{{ isset($pb) ? ' ' . $pb : ' pb-2' }}{{ isset($mt) ? ' ' . $mt : '' }}{{ isset($mb) ? ' ' . $mb : ' mb-0' }}{{ isset($class) : ' ' . $class : '' }}">
+<{{ $tag }} class="border-bottom border-gray{{ isset($pt) ? ' ' . $pt : '' }}{{ isset($pb) ? ' ' . $pb : ' pb-2' }}{{ isset($mt) ? ' ' . $mt : '' }}{{ isset($mb) ? ' ' . $mb : ' mb-0' }}{{ isset($class) ? ' ' . $class : '' }}">
     @if(isset($link))
         <a href="{{ $link }}" class="text-dark">
             {{ $title }}
