0e2c10231b033b3bce6d36190af50281d2401a16
Dan Anderson <anderson@familymedia.org>
2018-07-30 09:54:18 -0400

    About Page

--- src/resources/views/partials/about.blade.php
+++ src/resources/views/partials/about.blade.php
@@ -4,6 +4,6 @@
     'mb' => 'mb-3',
 ])
 <p class="text-muted small lh-135">
-    {{ config('job-core.domain') }} is a free {{ strtolower(config('job-core.keyword')) }} search tool powered by the USAJobs.gov API offered for free by Family Media LLC.
+    {{ config('job-core.domain') }} is a free {{ strtolower(config('job-core.keyword')) }} search tool powered by the <a href="https://developer.usajobs.gov/" target="_blank">USAJobs.gov API</a> offered for free by <a href="https://familymediallc.com/">Family Media LLC</a>.
 </p>
 @include('job-core::partials.resources')
