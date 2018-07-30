50e3a945982c2d379d6050bafebb26ce1a4c0cd3
Dan Anderson <anderson@familymedia.org>
2018-07-30 15:26:09 -0400

    Fix Filter Links

--- src/resources/views/partials/filter.blade.php
+++ src/resources/views/partials/filter.blade.php
@@ -5,9 +5,9 @@
 @foreach($locations as $child)
     @include('job-core::partials.p-tag', [
         'text' => $child->title,
-        'link' => $request->has('location') && $child->id === $request->location ? null : route($route, [
+        'link' => $request->has('location') && $child->id === (int) $request->location ? null : route($route, [
         	'location' => $child->id
-        ], $request->except('location')),
+        ] + $request->except('location')),
         'pt' => $loop->first ? 'pt-3' : 'pt-2',
         'pb' => $loop->last ? 'pb-4' : '',
     ])
@@ -21,7 +21,7 @@
         'text' => $child->value,
         'link' => $request->has('schedule') && $child->code === $request->schedule ? null : route($route, [
         	'schedule' => $child->code,
-        ], $request->except('schedule')),
+        ] + $request->except('schedule')),
         'pt' => $loop->first ? 'pt-3' : 'pt-2',
         'pb' => $loop->last ? 'pb-4' : '',
     ])
@@ -33,9 +33,9 @@
 @foreach($clearances as $child)
     @include('job-core::partials.p-tag', [
         'text' => $child->value,
-        'link' => $request->has('clearance') && $child->id === $request->clearance ? null : route($route, [
+        'link' => $request->has('clearance') && $child->id === (int) $request->clearance ? null : route($route, [
         	'clearance' => $child->id
-        ], $request->except('clearance')),
+        ] + $request->except('clearance')),
         'pt' => $loop->first ? 'pt-3' : 'pt-2',
         'pb' => '',
     ])
