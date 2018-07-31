e2cd28b6beaa206a3d6e36edfbb138cbb44c7da7
Dan Anderson <anderson@familymedia.org>
2018-07-30 21:08:55 -0400

    Shorten Description

--- src/resources/views/locations/partials/location.blade.php
+++ src/resources/views/locations/partials/location.blade.php
@@ -5,7 +5,7 @@
 ])
 @if(\Request::route()->getName() === 'listings.show')
     @include('job-core::partials.p-tag', [
-        'text' => strip_tags($location->description),
+        'text' => str_limit(strip_tags($location->description), 500),
         'pb' => 'pb-3 border-bottom border-gray',
     ])
     <small class="d-block text-right mt-3">
