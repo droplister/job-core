c7b67e2de99f5d89fd1da68cf13ee2788a118cb5
Dan Anderson <anderson@familymedia.org>
2018-07-31 18:09:12 -0400

    Tweaking Content

--- src/resources/views/partials/about.blade.php
+++ src/resources/views/partials/about.blade.php
@@ -7,6 +7,6 @@
     {{ config('job-core.domain') }} is a free {{ strtolower(config('job-core.keyword')) }} search tool powered by the <a href="https://developer.usajobs.gov/" target="_blank">USAJobs.gov API</a> offered for free by <a href="https://familymediallc.com/">Family Media LLC</a>. If you would like to get in touch with us about a problem, a business opportunity, or any other matter, please use our <a href="{{ route('contact.create') }}">contact form</a>.
 </p>
 <p class="text-muted small lh-135">
-    If this is your first time to {{ config('job-core.domain') }}, we recommend checking out our <a href="{{ route('alerts.index') }}">free job alerts</a>. And, in addition, we have listed below several resources which can help you while applying for {{ strtolower(config('job-core.keyword')) }}.
+    If this is your first time to {{ config('job-core.domain') }}, we recommend checking out our <a href="{{ route('alerts.index') }}">free job alerts</a>. And, in addition, we have listed several resources below which will help you apply for {{ strtolower(config('job-core.keyword')) }}, if you need.
 </p>
 @include('job-core::partials.resources')
