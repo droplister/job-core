0d10c5fffd88ad1acb582f2cd897c442c411a8b6
Dan Anderson <anderson@familymedia.org>
2018-07-31 13:36:59 -0400

    Simple Email Again

--- src/resources/views/emails/contact.blade.php
+++ src/resources/views/emails/contact.blade.php
@@ -1,18 +1,5 @@
-@component('mail::layout')
-	@slot('header')
-	    @component('mail::header', ['url' => config('app.url')])
-	        {{ config('job-core.domain') }}
-	    @endcomponent
-	@endslot
-
+@component('mail::message')
 	# {{ $first_name }} {{ $last_name }} said,
 
 	{{ $body }}
-
-    {{-- Footer --}}
-    @slot('footer')
-        @component('mail::footer')
-            &copy; {{ date('Y') }} Family Media LLC. All rights reserved.
-        @endcomponent
-    @endslot
 @endcomponent
