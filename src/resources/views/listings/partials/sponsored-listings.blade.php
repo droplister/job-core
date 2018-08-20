da0629e3442eb0dd99c7ee0b4d3ae9475733c575
Dan Anderson <anderson@familymedia.org>
2018-08-20 16:53:53 -0400

    Ignore Null

--- src/resources/views/listings/partials/sponsored-listings.blade.php
+++ src/resources/views/listings/partials/sponsored-listings.blade.php
@@ -1,10 +1,12 @@
-@if(auth()->check() && count($listings) === 0 || isset($sponsored))
-    @include('job-core::partials.h-tag', [
-        'tag' => 'h6',
-        'title' => 'Featured Jobs',
-    ])
-    @foreach($sponsored->all() as $job)
-        @include('job-core::listings.partials.sponsored-listing')
-        @if($loop->iteration == config('job-core.max_sponsored')) @break @endif
-    @endforeach
+@if($sponsored)
+    @if(auth()->check() && count($listings) === 0 || isset($sponsored))
+        @include('job-core::partials.h-tag', [
+            'tag' => 'h6',
+            'title' => 'Featured Jobs',
+        ])
+        @foreach($sponsored->all() as $job)
+            @include('job-core::listings.partials.sponsored-listing')
+            @if($loop->iteration == config('job-core.max_sponsored')) @break @endif
+        @endforeach
+    @endif
 @endif
